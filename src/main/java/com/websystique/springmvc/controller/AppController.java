2b131478caee31b6ca63958abb4589a9f3c31484
androsevgen <andros.evgen@gmail.com>
2016-09-02 18:04:41 +0300

    two

--- src/main/java/com/websystique/springmvc/controller/AppController.java
+++ src/main/java/com/websystique/springmvc/controller/AppController.java
@@ -6,31 +6,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 
-
 @Controller
 @RequestMapping("/")
 public class AppController {
 
-	@RequestMapping(value = { "/"}, method = RequestMethod.GET)
-	public String homePage(ModelMap model) {
-		return "home";
-	}
+    @RequestMapping(value = {"/"}, method = RequestMethod.GET)
+    public String homePage(ModelMap model) {
+        return "home";
+    }
 
-	@RequestMapping(value = { "/supervisors"}, method = RequestMethod.GET)
-	public String supervisorsPage(ModelMap model) {
-		return "supervisors";
-	}
-	@RequestMapping(value = { "/calculator"}, method = RequestMethod.GET)
-	public String calculatorPage(ModelMap model) {
-		return "calculator";
-	}
-	@RequestMapping(value = { "/game"}, method = RequestMethod.GET)
-	public String gamePage(ModelMap model) {
-		return "game";
-	}
+    @RequestMapping(value = {"/supervisors"}, method = RequestMethod.GET)
+    public String supervisorsPage(ModelMap model) {
+        return "supervisors";
+    }
 
-	@RequestMapping(value = { "/contactus"}, method = RequestMethod.GET)
-	public String contactUsPage(ModelMap model) {
-		return "contactus";
-	}
+    @RequestMapping(value = {"/calculator"}, method = RequestMethod.GET)
+    public String calculatorPage(ModelMap model) {
+        return "calculator";
+    }
+
+    @RequestMapping(value = {"/game"}, method = RequestMethod.GET)
+    public String gamePage(ModelMap model) {
+        return "game";
+    }
+
+    @RequestMapping(value = {"/contactus"}, method = RequestMethod.GET)
+    public String contactUsPage(ModelMap model) {
+        return "contactus";
+    }
 }
