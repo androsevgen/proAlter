ecf522431b0428302f8cb63b15b00c0e730bf8ce
andros.evgen <andros.evgen@gmail.com>
2016-09-08 21:22:05 +0300

    com1

--- src/main/java/com/proAlter/Top.java
+++ src/main/java/com/proAlter/Top.java
@@ -1,10 +1,9 @@
 package com.proAlter;
 
-
+import com.proAlter.conect.conect_to_bd;
 
 public class Top {
     public static void main(String[] args)  {
-
-
+        conect_to_bd con = new conect_to_bd();
     }
 }
--- src/main/java/com/proAlter/conect/conect_to_bd.java
+++ src/main/java/com/proAlter/conect/conect_to_bd.java
@@ -1,11 +1,48 @@
 package com.proAlter.conect;
 
+import org.firebirdsql.jdbc.FBDriver;
 
-
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class conect_to_bd {
-
-
+    public String getUrl() {
+        return url;
     }
+
+    public String getUser() {
+        return user;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void setConnection(Connection connection) {
+        this.connection = connection;
+    }
+
+    private final String url = "jdbc:firebirdsql:192.168.20.13/3050:alter_curent?lc_ctype=WIN1251;sql_dialect=3";
+    private final String user = "sysdba";
+    private final String pass = "sysadmin";
+    Connection connection;
+
+    public conect_to_bd() {
+
+        try {
+            Driver driver = new FBDriver();
+            DriverManager.registerDriver(driver);
+            connection = DriverManager.getConnection(url,user,pass);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
 
 
