ee67d69760865c39fb35ae420742b68254299842
andros.evgen <andros.evgen@gmail.com>
2016-09-08 22:32:34 +0300

    com1

--- src/main/java/com/proAlter/Top.java
+++ src/main/java/com/proAlter/Top.java
@@ -1,9 +1,12 @@
 package com.proAlter;
 
 import com.proAlter.conect.conect_to_bd;
-
+import  com.proAlter.conect.select_from_bd;
 public class Top {
     public static void main(String[] args)  {
         conect_to_bd con = new conect_to_bd();
+        select_from_bd d900 = new select_from_bd();
+        d900.rcd();
+
     }
 }
--- src/main/java/com/proAlter/conect/select_from_bd.java
+++ src/main/java/com/proAlter/conect/select_from_bd.java
@@ -1,12 +1,61 @@
 package com.proAlter.conect;
 
-/**
- * Created by admin on 08.09.2016.
- */
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
 public class select_from_bd {
-    public String rcd(){
-        conect_to_bd res = new  conect_to_bd();
-        String t700 = "";
+    public String rcd() {
+        conect_to_bd res = new conect_to_bd();
+        String t700 = "SELECT \n" +
+                "    ITEMCOLVO,\n" +
+                "    ITEMSUM,\n" +
+                "    ITEMVES,\n" +
+                "    AGENT,\n" +
+                "    DOCDATE,\n" +
+                "    UDFUMONTH(DOCDATE)||'.'||UDFUYEAR(DOCDATE) DATEMONTH,\n" +
+                "    GoodsGType,\n" +
+                "    CLIENTSNAME,\n" +
+                "    GOODSSNAME,\n" +
+                "    GOODSGROUPSNAME,\n" +
+                "    ADDRESSLOCATION\n" +
+                "from SPR_SELL2 (\n" +
+                " '01.08.2016',\n" +
+                " '06.08.2016',\n" +
+                " '*',\n" +
+                " '2008,2025,3030,4010,4011,5047,5048,560,2060,800,5060,5037,5046,5042,5061,5062,5065,5066,5067,5068,5069,5070,5072,801') order by DOCMONTH";
+
+        try {
+            Statement statement = res.getConnection().createStatement();
+            ResultSet resultSet = statement.executeQuery(t700);
+            while (resultSet.next()) {
+                double ITEMCOLVO = resultSet.getDouble("ITEMCOLVO");
+                double ITEMSUM = resultSet.getDouble("ITEMSUM");
+                double ITEMVES = resultSet.getDouble("ITEMVES");
+                String AGENT = resultSet.getString("AGENT");
+                String DOCDATE = resultSet.getString("DOCDATE");
+                String DATEMONTH = resultSet.getString("DATEMONTH");
+                String GoodsGType = resultSet.getString("GoodsGType");
+                String CLIENTSNAME = resultSet.getString("CLIENTSNAME");
+                String GOODSSNAME = resultSet.getString("GOODSSNAME");
+                String GOODSGROUPSNAME = resultSet.getString("GOODSGROUPSNAME");
+                String ADDRESSLOCATION = resultSet.getString("ADDRESSLOCATION");
+                System.out.println(ITEMCOLVO +
+                        ITEMSUM +
+                        ITEMVES +
+                        AGENT +
+                        DOCDATE +
+                        DATEMONTH +
+                        GoodsGType +
+                        CLIENTSNAME +
+                        GOODSSNAME +
+                        GOODSGROUPSNAME +
+                        ADDRESSLOCATION);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
         return null;
     }
