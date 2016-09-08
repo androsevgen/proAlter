d23c0849ba6c7beac056afcfc97907c81cafaa17
andros.evgen <andros.evgen@gmail.com>
2016-09-08 21:34:29 +0300

    com1

--- src/main/java/com/proAlter/conect/select_from_bd.java
+++ src/main/java/com/proAlter/conect/select_from_bd.java
@@ -4,4 +4,10 @@ package com.proAlter.conect;
  * Created by admin on 08.09.2016.
  */
 public class select_from_bd {
+    public String rcd(){
+        conect_to_bd res = new  conect_to_bd();
+        String t700 = "";
+
+        return null;
+    }
 }
