96214a69d24caeb9e8c2b46763ae1f3bba3b4a05
andros.evgen <andros.evgen@gmail.com>
2016-09-08 23:01:52 +0300

    com1

--- src/main/java/com/proAlter/Top.java
+++ src/main/java/com/proAlter/Top.java
@@ -4,7 +4,7 @@ import com.proAlter.conect.conect_to_bd;
 import  com.proAlter.conect.select_from_bd;
 public class Top {
     public static void main(String[] args)  {
-        conect_to_bd con = new conect_to_bd();
+        //conect_to_bd con = new conect_to_bd();
         select_from_bd d900 = new select_from_bd();
         d900.rcd();
 
