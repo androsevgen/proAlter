7a5e98b2820dc8f98f124c9ea144269598cda1bb
androsevgen <andros.evgen@gmail.com>
2016-09-23 18:07:40 +0300

    mda

--- src/main/java/com/proAlter/conect/select_from_bd.java
+++ src/main/java/com/proAlter/conect/select_from_bd.java
@@ -20,11 +20,7 @@ public class select_from_bd {
                 "    GOODSSNAME,\n" +
                 "    GOODSGROUPSNAME,\n" +
                 "    ADDRESSLOCATION\n" +
-                "from SPR_SELL2 (\n" +
-                " '01.08.2016',\n" +
-                " '06.08.2016',\n" +
-                " '*',\n" +
-                " '2008,2025,3030,4010,4011,5047,5048,560,2060,800,5060,5037,5046,5042,5061,5062,5065,5066,5067,5068,5069,5070,5072,801') order by DOCMONTH";
+                "from SPR_SELL2_RCD  order by DOCMONTH";
 
         try {
             Statement statement = res.getConnection().createStatement();
