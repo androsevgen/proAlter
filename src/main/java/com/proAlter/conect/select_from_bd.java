b146b2820ae1eb0d17ad94e89a038f3b3ce06ec5
androsevgen <andros.evgen@gmail.com>
2016-09-30 17:33:18 +0300

    mda

--- src/main/java/com/proAlter/conect/select_from_bd.java
+++ src/main/java/com/proAlter/conect/select_from_bd.java
@@ -1,11 +1,19 @@
 package com.proAlter.conect;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+import org.apache.commons.csv.QuoteMode;
+
+import java.io.FileWriter;
+import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 
 public class select_from_bd {
+
+
     public String rcd() {
         conect_to_bd res = new conect_to_bd();
         String t700 = "SELECT \n" +
@@ -37,7 +45,7 @@ public class select_from_bd {
                 String GOODSSNAME = resultSet.getString("GOODSSNAME");
                 String GOODSGROUPSNAME = resultSet.getString("GOODSGROUPSNAME");
                 String ADDRESSLOCATION = resultSet.getString("ADDRESSLOCATION");
-                System.out.println(ITEMCOLVO +" "+
+               /* System.out.println(ITEMCOLVO +" "+
                         ITEMSUM +" "+
                         ITEMVES +" "+
                         AGENT +" "+
@@ -47,7 +55,26 @@ public class select_from_bd {
                         CLIENTSNAME +" "+
                         GOODSSNAME +" "+
                         GOODSGROUPSNAME +" "+
-                        ADDRESSLOCATION);
+                        ADDRESSLOCATION);*/
+                try {
+                    CSVFormat csvFileFormat1 = CSVFormat.DEFAULT.withRecordSeparator("\n");
+
+                    FileWriter writer = new FileWriter("C:\\csv\\rcd.csv");
+                    CSVFormat csvFileFormat = CSVFormat.DEFAULT.withHeader("Кол-во;Сумма;КГ;Агент;Дата;Месяц;Тип товара;Клиент;Товар;Группа товара;Адрес факт").withEscape('\\').withQuoteMode(QuoteMode.NONE);
+                    CSVPrinter csvFilePrinter = new CSVPrinter(writer, csvFileFormat);
+
+                        csvFilePrinter.printRecord(ITEMCOLVO + ITEMSUM + ITEMVES + AGENT + DOCDATE + DATEMONTH +GoodsGType +CLIENTSNAME + GOODSSNAME + GOODSGROUPSNAME + ADDRESSLOCATION);
+                        writer.flush();
+                        writer.close();
+                        csvFilePrinter.close();
+
+
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+
             }
         } catch (SQLException e) {
             e.printStackTrace();
